548191ca967e0c53ce93cca3a813f095a8e6d48b
P12186619 RA <P12186619@my365.dmu.ac.uk>
2018-10-12 14:20:27 +0100

    added comments because even if its a hack it needs to look like it isnt.

--- lab1/simpleGame.cpp
+++ lab1/simpleGame.cpp
@@ -3,16 +3,16 @@
 #include <stdlib.h>
 #include <time.h>
 
-int compInt = 0;
-int guessInt = 0;
-int guesses = 8;
+int compInt = 0; // initilaiation for computer random int.
+int guessInt = 0; // initilaiation for user guess
+int guesses = 8; // Iniitialisation for amount of guesses
 
-int checkGuess();
+int checkGuess(); //Prototype function for guess handling
 
 int main()
 {
-	srand(time(NULL));
-	compInt = rand() % 1000 + 1;
+	srand(time(NULL)); //For truly random rand function
+	compInt = rand() % 1000 + 1; // number between 1 and 1000
 
 	
 	std::cout << "Random number=" << compInt << std::endl;
@@ -27,23 +27,23 @@ int main()
 }
 int checkGuess()
 {
-	if (guessInt == compInt)
+	if (guessInt == compInt) // if guess is correct
 	{
 		std::cout << "You are correct! Winner!" << std::endl;
-		guesses = 1;
+		guesses = 1; // cheat to stop program
 	}
 
-	if (guessInt < compInt)
+	if (guessInt < compInt) // if guess is less than random number
 	{
 		std::cout << "The guessed number is too Low,  Try again!" << std::endl;
 	}
-	if (guessInt > compInt)
+	if (guessInt > compInt) // if guess is more than random number
 	{
 		std::cout << "The guessed number is too High,  Try again!" << std::endl;
 	}
 
 	
-	guesses--;
+	guesses--; //handling amount of guesses left
 
 	std::cout << "You have guesses " << guesses << " remaining" << std::endl;
 	return guesses;
